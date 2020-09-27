11253d660e301bbac79adbd38d8affb990b53696
sametdumankaya <samety06@gmail.com>
2020-09-27 18:31:18 +0300

    visualization data send over bluetooth implementation

--- GaitAnalysisArduino/Mahony/IMUBluetoothSend/IMUBluetoothSend.ino
+++ GaitAnalysisArduino/Mahony/IMUBluetoothSend/IMUBluetoothSend.ino
@@ -1,11 +1,11 @@
 #include <Arduino_LSM9DS1.h>
 #include <ArduinoBLE.h>
-#include "SensorFusion.h"
+#include <MadgwickAHRS.h>
 
-SF fusion;
+Madgwick filter;
+unsigned long micros_per_reading, micros_previous;
+float accl_scale, gyro_scale;
 
-// count each send row
-int counter = 1;
 
 // IMU data will be formatted as a char array
 char imuData[500];
@@ -32,6 +32,10 @@ void setup() {
   if (!BLE.begin()) {
     while (1);
   }
+
+  filter.begin(119);
+  micros_per_reading = 1000000/119;
+  micros_previous = micros();
 
   // Local name will be shown on the devices that scanning for bluetooth devices.
   BLE.setLocalName("IMUMonitor");
@@ -60,28 +64,42 @@ void loop() {
     // While connection is established, publish IMU data with the connected device 
     while (central.connected()) {
       float gyx, gyy, gyz, acx, acy, acz, mgx, mgy, mgz;
+      float roll, pitch, heading;
+      unsigned long micros_now;
+      micros_now = micros();
+      if (micros_now - micros_previous >= micros_per_reading){
+        IMU.readGyroscope(gyx, gyy, gyz);
+        IMU.readAcceleration(acx, acy, acz);
+        IMU.readMagneticField(mgx, mgy, mgz);
+  
+        //filter.update(gyx, gyy, gyz, acx, acy, acz, mgx, mgy, mgz); //for all 3
+        filter.updateIMU(gyx, gyy, gyz, acx, acy, acz); //only for accl, gyro
+        roll = filter.getRoll();
+        pitch = filter.getPitch();
+        heading = filter.getYaw();
+          
+        micros_previous = micros_previous + micros_per_reading;
+  
+        // Divide magnetic field values by 100, since sensor returns values expressed in microtesla (uT) and
+        // we need Gauss (G).
+        // Format the char array as wanted (Comma Seperated Values)
+        // With this operation, imuData variable has all the IMU information
+        //sprintf(imuData, "%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f", 
+        //        gyx, gyy, gyz, acx, acy, acz, mgx / 100, mgy / 100, mgz / 100);
+  
+        sprintf(imuData, "%.5f,%.5f,%.5f", heading, pitch, roll);
+  
+        // Need to convert char array into String, then convert it into Byte array
+        // So we can publish it via bluetooth.
+        dataStr = imuData;
+        byte bytes[dataStr.length() + 1];
+        dataStr.getBytes(bytes, dataStr.length() + 1);
+  
+        // Update the characteristic's value
+        imuCharacteristic.writeValue(bytes, sizeof(bytes));
+      }
     
-      IMU.readGyroscope(gyx, gyy, gyz);
-      IMU.readAcceleration(acx, acy, acz);
-      IMU.readMagneticField(mgx, mgy, mgz);
-
-      // Divide magnetic field values by 100, since sensor returns values expressed in microtesla (uT) and
-      // we need Gauss (G).
-      // Format the char array as wanted (Comma Seperated Values)
-      // With this operation, imuData variable has all the IMU information
-      sprintf(imuData, "%d,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f", 
-              counter, gyx, gyy, gyz, acx, acy, acz, mgx / 100, mgy / 100, mgz / 100);
-
-      // Need to convert char array into String, then convert it into Byte array
-      // So we can publish it via bluetooth.
-      dataStr = imuData;
-      byte bytes[dataStr.length() + 1];
-      dataStr.getBytes(bytes, dataStr.length() + 1);
-
-      // Update the characteristic's value
-      imuCharacteristic.writeValue(bytes, sizeof(bytes));
-      Serial.println(dataStr); 
-      counter++;
+      
     }
 
     // When the connected device is disconnected, turn the led off.
